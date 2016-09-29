99b131dbf7889609981e45a0978cf40e62cc1865
Nyoh <iivan1986@gmail.com>
2016-09-29 00:10:50 -0500

    get page

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,39 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+
+    QNetworkAccessManager* manager = new QNetworkAccessManager(this);
+    QString url = ui->plainTextEdit->toPlainText();
+    m_Reply = manager->get( QNetworkRequest( QUrl(url) ));
+    connect( m_Reply, SIGNAL(readyRead()), this, SLOT(onRequestFinished()) );
+    connect( m_Reply, SIGNAL(error(QNetworkReply::NetworkError)), this, SLOT(error(QNetworkReply::NetworkError)) );
+    connect( m_Reply, SIGNAL(sslErrors(const QList<QSslError>&)), this, SLOT(sslErrors(const QList<QSslError>&)) );
+
+
+}
+
+void MainWindow::onRequestFinished()
+{
+if( ! m_Reply )
+return;
+
+if( m_Reply->error() == QNetworkReply::NoError )
+{
+    ui->textEdit->setText(m_Reply->readAll());
+}
+m_Reply->deleteLater();
+
+}
+
+void MainWindow::error(QNetworkReply::NetworkError code)
+{
+
+}
+
+void MainWindow::sslErrors(const QList<QSslError> & errors)
+{
+
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtNetwork/qnetworkreply.h>
+#include <QPointer>
 
 namespace Ui {
     class MainWindow;
@@ -15,8 +17,19 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+    void onRequestFinished();
+    void error(QNetworkReply::NetworkError code);
+    void sslErrors(const QList<QSslError> & errors);
+
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QPointer<QNetworkReply> m_Reply;
+
+
 };
 
 #endif // MAINWINDOW_H
